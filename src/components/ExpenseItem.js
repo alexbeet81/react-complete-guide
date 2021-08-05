f32acf60a92ee9adec605c7f3d4090eda54bc67c
Alex Pritchard <alexbeet1234@gmail.com>
2021-08-05 15:02:30 +0800

    updated expensitem file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+  return (
+    <div>
+      <div>August 4th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>£295.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
