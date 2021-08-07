fb19b49f3ddcdee3590e38eda312623a068f736e
Alex Pritchard <alexbeet1234@gmail.com>
2021-08-07 10:14:58 +0800

    created new expense folder and created new expense.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
  
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickedHandler = () => {
+    setTitle("hello");
+  } 
+
   return ( 
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
+      <button onClick={clickedHandler}>Change Title</button>
     </Card>
   );
 }
