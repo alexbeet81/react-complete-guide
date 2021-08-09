813b97af6788cc40211249c13231a07e7c102aa9
Alex Pritchard <alexbeet1234@gmail.com>
2021-08-09 20:20:33 +0800

    created chart and chart bar files

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,14 +1,22 @@
-import { props } from 'bluebird';
-import { defaultMinSockets } from 'forever-agent';
-import React from 'react';
+import { props } from "bluebird";
+import { defaultMinSockets } from "forever-agent";
+import React from "react";
 
-import ChartBar from './ChartBar';
-import './ChartBar.css';
+import ChartBar from "./ChartBar";
+import "./ChartBar.css";
 
 const Chart = () => {
-    return <div className="chart">
-        {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value}/>)
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={null}
+          lable={dataPoint.label}
+        />
+      ))}
     </div>
-}
+  );
+};
 
 export default Chart;
